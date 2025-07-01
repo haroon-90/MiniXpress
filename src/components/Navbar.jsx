e655cde8c1dadfebd6bc5cc2f3eee9bb4a6334dc
haroon-90 <haroonboy90@gmail.com>
2025-07-01 12:59:52 +0500

    fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,62 +13,64 @@ function Navbar() {
     const handleMenuToggle = () => setMenuOpen((open) => !open);
 
     return (
-        <nav className="px-8 py-4 flex justify-between items-center backdrop-blur-[4px] border-b relative rounded-b-2xl">
-            <div className="flex gap-2 justify-center items-center">
-                <img src={LogoIcon} alt="logo" className='h-12' />
-                <img src={LogoText} alt="logo" className='h-5' />
-            </div>
-            <div className='flex md:justify-between justify-end items-center w-[65%]'>
-                <div className="hidden md:flex space-x-4">
-                    <Link className="hover:text-[#FFA500] relative inline-block after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-[#FFA500] after:transition-all after:duration-300 hover:after:w-full " to="/MiniXpress/games">Games</Link>
-                    <Link className="hover:text-[#FFA500] relative inline-block after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-[#FFA500] after:transition-all after:duration-300 hover:after:w-full " to="/MiniXpress/utilities">Utilities</Link>
-                    <Link className="hover:text-[#FFA500] relative inline-block after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-[#FFA500] after:transition-all after:duration-300 hover:after:w-full " to="/MiniXpress/entertainment">Entertainment</Link>
-                    <Link className="hover:text-[#FFA500] relative inline-block after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-[#FFA500] after:transition-all after:duration-300 hover:after:w-full " to="/MiniXpress/randomizers">Randomizers</Link>
+        <nav className="px-8 py-4 backdrop-blur-[4px] border-b relative rounded-b-2xl">
+            <div className='max-w-7xl mx-auto flex justify-between items-center'>
+                <div className="flex gap-2 justify-center items-center">
+                    <img src={LogoIcon} alt="logo" className='h-12' />
+                    <img src={LogoText} alt="logo" className='h-5' />
                 </div>
-                <button title='Theme' onClick={toggleTheme} className="px-2 py-1 border rounded-full ml-4">
-                    {isDark ? <FaSun /> : <FaMoon />}
+                <div className='flex md:justify-between justify-end items-center w-[65%]'>
+                    <div className="hidden md:flex space-x-4">
+                        <Link className="hover:text-[#FFA500] relative inline-block after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-[#FFA500] after:transition-all after:duration-300 hover:after:w-full " to="/MiniXpress/games">Games</Link>
+                        <Link className="hover:text-[#FFA500] relative inline-block after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-[#FFA500] after:transition-all after:duration-300 hover:after:w-full " to="/MiniXpress/utilities">Utilities</Link>
+                        <Link className="hover:text-[#FFA500] relative inline-block after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-[#FFA500] after:transition-all after:duration-300 hover:after:w-full " to="/MiniXpress/entertainment">Entertainment</Link>
+                        <Link className="hover:text-[#FFA500] relative inline-block after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-[#FFA500] after:transition-all after:duration-300 hover:after:w-full " to="/MiniXpress/randomizers">Randomizers</Link>
+                    </div>
+                    <button title='Theme' onClick={toggleTheme} className="px-2 py-1 border rounded-full ml-4">
+                        {isDark ? <FaSun /> : <FaMoon />}
+                    </button>
+                </div>
+                <button
+                    className="md:hidden ml-4 z-20"
+                    onClick={handleMenuToggle}
+                    aria-label="Toggle menu"
+                >
+                    {menuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
                 </button>
+
+                {menuOpen && (
+                    <div className={`absolute top-full right-0 mt-2 w-48 ${isDark ? "bg-white text-black" : "bg-blue-900 text-white"} shadow-lg rounded-md flex flex-col md:hidden z-100`}>
+                        <Link
+                            to="/MiniXpress/games"
+                            className={`px-4 py-2 ${isDark ? "hover:bg-gray-200" : "hover:bg-blue-700"}`}
+                            onClick={() => setMenuOpen(false)}
+                        >
+                            Games
+                        </Link>
+                        <Link
+                            to="/MiniXpress/utilities"
+                            className={`px-4 py-2 ${isDark ? "hover:bg-gray-200" : "hover:bg-blue-700"}`}
+                            onClick={() => setMenuOpen(false)}
+                        >
+                            Utilities
+                        </Link>
+                        <Link
+                            to="/MiniXpress/entertainment"
+                            className={`px-4 py-2 ${isDark ? "hover:bg-gray-200" : "hover:bg-blue-700"}`}
+                            onClick={() => setMenuOpen(false)}
+                        >
+                            Entertainment
+                        </Link>
+                        <Link
+                            to="/MiniXpress/randomizers"
+                            className={`px-4 py-2 ${isDark ? "hover:bg-gray-200" : "hover:bg-blue-700"}`}
+                            onClick={() => setMenuOpen(false)}
+                        >
+                            Randomizers
+                        </Link>
+                    </div>
+                )}
             </div>
-            <button
-                className="md:hidden ml-4 z-20"
-                onClick={handleMenuToggle}
-                aria-label="Toggle menu"
-            >
-                {menuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
-            </button>
-            
-            {menuOpen && (
-                <div className={`absolute top-full right-0 mt-2 w-48 ${isDark ? "bg-white text-black" : "bg-blue-900 text-white"} shadow-lg rounded-md flex flex-col md:hidden z-10`}>
-                    <Link
-                        to="/MiniXpress/games"
-                        className={`px-4 py-2 ${isDark ? "hover:bg-gray-200" : "hover:bg-blue-700"}`}
-                        onClick={() => setMenuOpen(false)}
-                    >
-                        Games
-                    </Link>
-                    <Link
-                        to="/MiniXpress/utilities"
-                        className={`px-4 py-2 ${isDark ? "hover:bg-gray-200" : "hover:bg-blue-700"}`}
-                        onClick={() => setMenuOpen(false)}
-                    >
-                        Utilities
-                    </Link>
-                    <Link
-                        to="/MiniXpress/entertainment"
-                        className={`px-4 py-2 ${isDark ? "hover:bg-gray-200" : "hover:bg-blue-700"}`}
-                        onClick={() => setMenuOpen(false)}
-                    >
-                        Entertainment
-                    </Link>
-                    <Link
-                        to="/MiniXpress/randomizers"
-                        className={`px-4 py-2 ${isDark ? "hover:bg-gray-200" : "hover:bg-blue-700"}`}
-                        onClick={() => setMenuOpen(false)}
-                    >
-                        Randomizers
-                    </Link>
-                </div>
-            )}
         </nav>
     )
 }
--- src/parts/card.jsx
+++ src/parts/card.jsx
@@ -12,7 +12,7 @@ const Card = (prop) => {
   return (
     <Link
       to={lowertitle}
-      className={`flex flex-col justify-center items-center bg-[#FFA500] md:h-[30vh] md:w-[20vw] h-[40vw] min-w-[14vh] text-center p-6 rounded-[2rem] transition-transform duration-300 ease-out transform hover:-translate-y-2 
+      className={`flex flex-col justify-center items-center bg-[#FFA500] md:h-[30vh] md:w-[20vw] h-[40vw] min-w-[40vw] md:min-w-[14vh] text-center p-6 rounded-[2rem] transition-transform duration-300 ease-out transform hover:-translate-y-2 
         ${isDark
           ? "shadow-[0_20px_50px_rgba(255,165,0,0.3)] hover:shadow-[0_30px_60px_rgba(255,165,0,0.5)]"
           : "shadow-[0_20px_50px_rgba(255,209,124,0.3)] hover:shadow-[0_30px_60px_rgba(255,209,124,0.5)]"
@@ -21,9 +21,9 @@ const Card = (prop) => {
       <img
         src={icon || Logo}
         alt={lowertitle}
-        className={`w-20 h-20 mb-4 transition-transform duration-300 ${isDark ? "invert" : ""} hover:scale-110`}
+        className={`h-15 md:h-20 mb-4 transition-transform duration-300 ${isDark ? "invert" : ""} hover:scale-110`}
       />
-      <span className="text-2xl font-bold tracking-wide drop-shadow-md">
+      <span className="md:text-2xl text-lg font-bold tracking-wide drop-shadow-md">
         {title}
       </span>
     </Link>
